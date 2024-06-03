e676a32789dfae21a4932d6c9357611eccda3e15
agat <abdulghaniasykurthoriq@gmail.com>
2024-06-03 10:49:28 +0700

    deployment 1.0.0

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask, request, jsonify
-import flask
-
-print("Flask version:", flask.__version__)
-
 import joblib
 import re
-import requests
-import os
 
 app = Flask(__name__)
 
@@ -20,12 +14,15 @@ vectorizer = joblib.load(vectorizer_filename)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    text = data['text']
-    processed_text = preprocess_text(text)
-    vectorized_text = vectorizer.transform([processed_text])
-    prediction = model.predict(vectorized_text)
-    return jsonify({'prediction': prediction[0]})
+    if request.is_json:
+        data = request.get_json()
+        text = data['text']
+        processed_text = preprocess_text(text)
+        vectorized_text = vectorizer.transform([processed_text])
+        prediction = model.predict(vectorized_text)
+        return jsonify({'prediction': prediction[0]})
+    else:
+        return jsonify({"error": "Invalid Content-Type"}), 415
 
 def preprocess_text(text):
     text = text.lower()
